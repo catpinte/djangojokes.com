da79aa3acbfb9c116afb252fd65343471a811452
cpinte <catherine.pinte@luxairgroup.lu>
2026-03-02 14:23:27 +0100

    JokeVote

--- jokes/models.py
+++ jokes/models.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Avg
 from django.urls import reverse
 
 from common.utils.text import unique_slug
 
 class Joke(models.Model):
     question = models.TextField(max_length=200)
-    answer = models.TextField(max_length=100, blank=True)
+    answer = models.TextField(max_length=100)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.PROTECT 
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT
     )
     category = models.ForeignKey(
         'Category', on_delete=models.PROTECT
@@ -20,60 +21,83 @@ class Joke(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    def get_absolute_url(self): 
+    def get_absolute_url(self):
         return reverse('jokes:detail', args=[self.slug])
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             value = str(self)
             self.slug = unique_slug(value, type(self))
-            
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.question
-    
+
+
 class Category(models.Model):
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
     def get_absolute_url(self):
-        return reverse('jokes:category', args=[self.slug])
-    
+        return reverse('category', args=[self.slug])
+
     def save(self, *args, **kwargs):
         if not self.slug:
             value = str(self)
             self.slug = unique_slug(value, type(self))
-            super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.category
-    
-    class Meta: 
-        verbose_name_plural = 'Categories'
-        ordering = ['category']
 
-class Tag(models.Model): 
-    tag = models.CharField(max_length=50) 
-    slug = models.SlugField( max_length=50, unique=True, null=False, editable=False 
-    ) 
-    created = models.DateTimeField(auto_now_add=True) 
-    updated = models.DateTimeField(auto_now=True) 
-    
-    def get_absolute_url(self): 
-        return reverse('jokes:tag', args=[self.slug]) 
-    
-    def save(self, *args, **kwargs): 
-        if not self.slug: 
-            value = str(self) 
-            self.slug = unique_slug(value, type(self)) 
-        super().save(*args, **kwargs) 
-    
-    def __str__(self): 
-        return self.tag    
-    
-    class Meta: ordering = ['tag']
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('tag', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        ordering = ['tag']
+
+
+class JokeVote(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    joke = models.ForeignKey(
+        Joke, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    vote = models.SmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'joke'], name='one_vote_per_user_per_joke'
+            )
+        ]
