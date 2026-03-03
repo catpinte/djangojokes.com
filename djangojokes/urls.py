68696a31dbfb35d16f7b9308c713412f60efc763
cpinte <catherine.pinte@luxairgroup.lu>
2026-03-03 14:44:05 +0100

    Ex 26

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -15,3 +16,8 @@ urlpatterns = [
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
 ]
+if settings.DEBUG: 
+    import debug_toolbar 
+    urlpatterns = [ 
+        path('__debug__/', include(debug_toolbar.urls)), 
+        ] + urlpatterns
